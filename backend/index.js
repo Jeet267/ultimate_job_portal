c2e2d01e5b9ae5d51dcb159c80293de451dda437
kanhaiyak23 <kanhaiyakumarsingh086@gmail.com>
2024-11-07 04:34:36 +0530

    production level

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
-
+import path from 'path';
 dotenv.config({});
 
 const app = express();
@@ -25,12 +25,16 @@ app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
-
+const __dirname = path.resolve()
 // api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/frontend/dist/index.html"));
+})
 
 
 
