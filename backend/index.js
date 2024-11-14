fb2d6fcc88b10135fd2e8e22c31fa76ce87cc6c6
Jeet267 <ajtkr200@gmail.com>
2024-11-14 23:40:53 +0530

    2

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://ultimate-job-portal.onrender.com',
+    origin:['http://localhost:5173'],
     credentials:true
 }
 
