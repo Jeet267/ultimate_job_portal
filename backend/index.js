5cabc2bb80df539e187980681723abfb8a48908a
Jeet267 <ajtkr200@gmail.com>
2024-11-14 23:09:06 +0530

    2

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://job-portal-mz2k.onrender.com',
+    origin:'https://localhost:5173',
     credentials:true
 }
 
